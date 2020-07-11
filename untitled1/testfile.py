43b43261ed9049b47373182e42357386e9a30963
Shoukat Malik <shaukat.bahrimalik@gmail.com>
2020-07-11 16:13:33 +0500

    Added fitoki and fiveseasons url completed

--- untitled1/testfile.py
+++ untitled1/testfile.py
@@ -30,18 +30,126 @@ time.sleep(2.4)
 #driver.find_element_by_xpath('//*[@id="header"]/div[2]/div/div[2]/div[3]/div[1]/a/span').click()
 
 #creating Dictinaries
-person_dict = json.loads(sys.argv[1])
-isNewUser_dict = person_dict['isNewUser'];
-email_dict = person_dict['PatientEmail'];
-pass_dict = person_dict['PatientVATnumber'];
+order_dict = json.loads(sys.argv[1])
+isNewUser_dict = order_dict['isNewUser'];
+email_model = order_dict['PatientEmail'];
+password_model = order_dict['PatientVATnumber'];
+product_model = order_dict['products'];
+
+#provider = product_model['providerName'];
+#pinting Models start --
+print(order_dict)
+print(email_model)
+print(password_model)
+print(product_model)
+
+#print(provider)
+#model printing end---
 time.sleep(2.4)
 if isNewUser_dict == 'false':
     driver.get('https://www.fitotouch.com/account/login')
     time.sleep(2.4)
-    driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[1]/div/input').send_keys('shoukatmalik@gmail.com')
-    driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/input').send_keys('danat123$')
+    driver.switch_to.frame("accountFrame")
+    time.sleep(2.4)
+    driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[1]/div/input').send_keys(email_model)
+    driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/input').send_keys(password_model)
     driver.find_element_by_xpath('//*[@id="root"]/div/div/div/button').click()
-    driver.get("https://www.fitotouch.com/fitoki/f-001-jing-fang-bai-du-wan")
+    time.sleep(2.4)
+    # redirect to home page
+    driver.get(base_url)
+    for name in product_model:
+        fitoName = name.get('fitoName');
+        fitoCode = name.get('fitoCode');
+        fitoQuantity = name.get('fitoQuantity');
+        provider = name.get('providerName');
+        providerCode = name.get('providerCode');
+        # category Naming start
+        # filter categories
+        print(provider)
+        if provider.lower() == 'fiveseasons':
+            x = fitoName.split('-')
+            y = len(x)
+            third = x[1].split()
+            fito_length = len(third)
+            if fito_length <= 1:
+                fito_url = "{}-{}".format(x[0], third[0])
+                print(fito_url)
+            elif fito_length <= 2:
+                fito_url = "{}-{}-{}".format(x[0], third[0], third[1])
+                print(fito_url)
+            elif fito_length <= 3:
+                fito_url = "{}-{}-{}-{}".format(x[0], third[0], third[1], third[2])
+                print(fito_url)
+            elif fito_length <= 4:
+                fito_url = "{}-{}-{}-{}-{}".format(x[0], third[0], third[1], third[2], third[3])
+                print(fito_url)
+            elif fito_length <= 5:
+                fito_url = "{}-{}-{}-{}-{}-{}".format(x[0], third[0], third[1], third[2], third[3], third[4])
+                print(fito_url)
+            elif fito_length <= 6:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}".format(x[0], third[0], third[1], third[2], third[3], third[4], third[5])
+                print(fito_url)
+            elif fito_length <= 7:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}-{}".format(x[0], third[0], third[1], third[2], third[3], third[4],third[5], third[6])
+                print(fito_url)
+            elif fito_length <= 8:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}-{}-{}".format(x[0], third[0], third[1], third[2], third[3], third[4],third[5], third[6], third[7])
+                print(fito_url)
+            elif fito_length <= 9:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}-{}-{}-{}".format(x[0], third[0], third[1], third[2], third[3],third[4], third[5], third[6], third[7], third[8])
+                print(fito_url)
+            driver.get('https://www.fitotouch.com/fiveseasons')
+        elif provider.lower() == 'giovanni-maciocia':
+            driver.get('https://www.fitotouch.com/giovanni-maciocia')
+        elif provider.lower() == 'soria-composor':
+            driver.get('https://www.fitotouch.com/soria-composor')
+        elif provider.lower() == 'soria-chinasor':
+            driver.get('https://www.fitotouch.com/soria-chinasor')
+        elif provider.lower() == 'bluepoppy':
+            driver.get('https://www.fitotouch.com/bluepoppy')
+        elif provider.lower() == 'fitoki':
+            x = fitoName.split('-')
+            y = len(x)
+            third = x[2].split()
+            fito_length = len(third)
+            if fito_length <= 1:
+                fito_url = "{}-{}-{}".format(x[0],x[1], third[0])
+                print(fito_url)
+            elif fito_length <= 2:
+                fito_url = "{}-{}-{}-{}".format(x[0],x[1], third[0], third[1])
+                print(fito_url)
+            elif fito_length <= 3:
+                fito_url = "{}-{}-{}-{}-{}".format(x[0],x[1], third[0], third[1], third[2])
+                print(fito_url)
+            elif fito_length <= 4:
+                fito_url = "{}-{}-{}-{}-{}-{}".format(x[0],x[1], third[0], third[1], third[2], third[3])
+                print(fito_url)
+            elif fito_length <= 5:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}".format(x[0],x[1], third[0], third[1], third[2], third[3], third[4])
+                print(fito_url)
+            elif fito_length <= 6:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}-{}".format(x[0],x[1], third[0], third[1], third[2], third[3], third[4], third[5])
+                print(fito_url)
+            elif fito_length <= 7:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}-{}-{}".format(x[0],x[1], third[0], third[1], third[2], third[3], third[4],third[5], third[6])
+                print(fito_url)
+            elif fito_length <= 8:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}-{}-{}-{}".format(x[0],x[1], third[0], third[1], third[2], third[3], third[4],third[5], third[6], third[7])
+                print(fito_url)
+            elif fito_length <= 9:
+                fito_url = "{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}".format(x[0],x[1], third[0], third[1], third[2], third[3],third[4], third[5], third[6], third[7], third[8])
+                print(fito_url)
+            driver.get('https://www.fitotouch.com/fitoki')
+            time.sleep(2.4)
+    #driver.get("https://www.fitotouch.com/fitoki/f-001-jing-fang-bai-du-wan")
+    order_url = "{}/{}/{}".format(base_url, provider.lower(), fito_url.lower())
+    driver.get(order_url.replace(' ', ''))
+    driver.execute_script("window.scrollTo(0, 150)")
+    time.sleep(2.4)
+    driver.find_element_by_xpath('/html/body/div[1]/main/article/section/div[2]/div/section/article/section[1]/section/div/div[3]/div/div').click()
+    time.sleep(2.4)
+    driver.get('https://www.fitotouch.com/cart')
+
 else :
  driver.get('https://www.fitotouch.com/account/login/create')
  time.sleep(2.4)
